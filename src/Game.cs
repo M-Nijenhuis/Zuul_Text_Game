ec55be58f052375201c8c263cb123dcd2e396d5f
MichielN19 <Michielmitsjol@gmail.com>
2025-02-26 10:33:40 +0100

    Added damage when move room and you die when health is zero or below

--- src/Game.cs
+++ src/Game.cs
@@ -4,12 +4,13 @@ class Game
 {
   // Private fields
   private Parser parser;
-  private Room currentRoom;
+  private Player player;
 
   // Constructor
   public Game()
   {
     parser = new Parser();
+    player = new Player();
     CreateRooms();
   }
 
@@ -43,7 +44,7 @@ class Game
     // ...
 
     // Start game outside
-    currentRoom = outside;
+    player.CurrentRoom = outside;
   }
 
   //  Main play routine. Loops until end of play.
@@ -54,7 +55,7 @@ class Game
     // Enter the main command loop. Here we repeatedly read commands and
     // execute them until the player wants to quit.
     bool finished = false;
-    while (!finished)
+    while (!finished && player.IsAlive() == true) 
     {
       Command command = parser.GetCommand();
       finished = ProcessCommand(command);
@@ -72,7 +73,7 @@ class Game
     Console.WriteLine("Zuul is a new, incredibly boring adventure game.");
     Console.WriteLine("Type 'help' if you need help.");
     Console.WriteLine();
-    Console.WriteLine(currentRoom.GetLongDescription());
+    Console.WriteLine(player.CurrentRoom.GetLongDescription());
   }
 
   // Given a command, process (that is: execute) the command.
@@ -101,6 +102,9 @@ class Game
         break;
       case "look":
         Look();
+        break;
+      case "status":
+        Status();
         break;
     }
 
@@ -136,21 +140,28 @@ class Game
     string direction = command.SecondWord;
 
     // Try to go to the next room.
-    Room nextRoom = currentRoom.GetExit(direction);
+    Room nextRoom = player.CurrentRoom.GetExit(direction);
     if (nextRoom == null)
     {
       Console.WriteLine("There is no door to " + direction + "!");
       return;
     }
 
-    currentRoom = nextRoom;
-    Console.WriteLine(currentRoom.GetLongDescription());
+    player.CurrentRoom = nextRoom;
+    Console.WriteLine(player.CurrentRoom.GetLongDescription());
+    player.Damage(10);
   }
 
 
   private void Look()
   {
-    Console.WriteLine($"{currentRoom.GetLongDescription()}");
+    Console.WriteLine($"{player.CurrentRoom.GetLongDescription()}");
+  }
+
+  private void Status()
+  {
+    Console.WriteLine($"You're health is {player.health}/100");
   }
 }
+
 
